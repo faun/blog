98f75eb204ea4dd9adfbdb0fbb22285fb2b31f01
Faun <faun@riseup.net>
2014-02-15 19:43:45 +0700

    Make quotes consistent

--- config.rb
+++ config.rb
@@ -3,29 +3,29 @@
 # Blog settings
 ###
 
-Time.zone = "Bangkok"
+Time.zone = 'Bangkok'
 
 activate :blog do |blog|
 
-  # blog.permalink = ":year/:month/:day/:title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
-  # blog.taglink = "tags/:tag.html"
+  # blog.permalink = ':year/:month/:day/:title.html'
+  # blog.sources = ':year-:month-:day-:title.html'
+  # blog.taglink = 'tags/:tag.html'
 
-  blog.layout = "layouts/post.html.haml"
+  blog.layout = 'layouts/post.html.haml'
 
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  blog.default_extension = ".md"
+  # blog.year_link = ':year.html'
+  # blog.month_link = ':year/:month.html'
+  # blog.day_link = ':year/:month/:day.html'
+  blog.default_extension = '.md'
 
   # blog.paginate = true
   # blog.per_page = 10
-  # blog.page_link = "page/:num"
+  # blog.page_link = 'page/:num'
 end
 
-page "/feed.xml", :layout => false
+page '/feed.xml', :layout => false
 
 ###
 # Compass
@@ -42,19 +42,19 @@ page "/feed.xml", :layout => false
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page '/path/to/file.html', :layout => false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page '/path/to/file.html', :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page "/admin/*"
+#   page '/admin/*'
 # end
 
 # Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
+# page '/this-page-has-no-template.html', :proxy => '/template-file.html' do
+#   @which_fake_page = 'Rendering a fake page with a variable'
 # end
 
 ###
@@ -67,7 +67,7 @@ page "/feed.xml", :layout => false
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 require 'ostruct'
@@ -75,7 +75,7 @@ require 'yaml'
 
 class SiteSettings < Middleman::Extension
   def initialize(app, options_hash={}, &block)
-    settings = OpenStruct.new YAML::load File.read "_config.yml"
+    settings = OpenStruct.new YAML::load File.read '_config.yml'
 
     app.set :site, settings
     super
@@ -125,11 +125,11 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
+  # require 'middleman-smusher'
   # activate :smusher
 
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_path, '/Content/images/'
 end
 
 require 'rack/offline'
@@ -137,15 +137,15 @@ require 'rack/offline'
 ready do
   all_pages = sitemap.resources.map{|r| r.destination_path }
   offline = Rack::Offline.configure do
-    cache "http://code.jquery.com/jquery.min.js"
-    cache "http://html5shim.googlecode.com/svn/trunk/html5.js"
+    cache 'http://code.jquery.com/jquery.min.js'
+    cache 'http://html5shim.googlecode.com/svn/trunk/html5.js'
     fallback '/' =>  '/offline.html'
     network 'http://www.google-analytics.com/ga.js'
     network 'http://www.google-analytics.com/__utm.gif'
     all_pages.each {|page| cache page }
   end
 
-  map("/offline.appcache") { run offline }
-  endpoint '/offline.appcache', :path => "/offline.appcache"
+  map('/offline.appcache') { run offline }
+  endpoint 'offline.appcache', :path => '/offline.appcache'
 end
 
